12732a71aedcd7b130eb36acc9c45be7182a6e18
Hans Ekbrand <hans.ekbrand@gmail.com>
2020-08-18 12:15:16 +0200

    experiment with batch size 1500 for gpu-monster

--- script_for_gpu-monster.sh
+++ script_for_gpu-monster.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-## prerequisites to run this script
-# 1. a lc0 recent official binary in ~/src/lc0/build/release/lc0
-# 2. the lczero-training repo available in ~/src (clone/fork from git@github.com:hans-ekbrand/lczero-training.git)
-# 3. a file foo.yaml in ~/src/lczero-training/tf/configs/ for this particular experiment
-# 4 tensorflow in a docker
+## This is a special case scenario, where the tensorflow binary is only available in a docker container. This was the only way to compile tensorflow for a CPU without AVX.
 
 ## name for this experiment
 variant=bar
@@ -12,6 +8,7 @@ variant=bar
 mkdir -p ~/leela-training-games/${variant}/.cache/lc0
 
 XDG_CACHE_HOME=$HOME/leela-training-games/${variant}
+export $XDG_CACHE_HOME
 
 if [[  -z $1 ]]; then
     echo "Required parameter counter missing. To start over use 1"
@@ -34,21 +31,15 @@ until false # run this loop until further notice
   latest_net=`ls -Str ${net_dir} | tail -n 1`
   if [[ -z $latest_net ]]; then
       ## No net available yet
-      $REAL_HOME/src/lc0/build/release/lc0 selfplay --training --games=$number_of_games_per_net --parallelism=12 --visits=800 --backend=random
+      $HOME/src/lc0/build/release/lc0 selfplay --training --games=$number_of_games_per_net --parallelism=12 --visits=800 --backend=random
   else
       # generate new training data using the latest net
       $HOME/src/lc0/build/release/lc0 selfplay -w ${net_dir}/${latest_net} --training --games=$number_of_games_per_net --backend-opts="(backend=cudnn,gpu=0),(backend=cudnn,gpu=1),(backend=cudnn,gpu=2),(backend=cudnn,gpu=3),(backend=cudnn,gpu=4),(backend=cudnn,gpu=5)" --parallelism=12 --visits=10000 --cpuct=1.32 --cpuct-at-root=1.9 --root-has-own-cpuct-params=true --resign-percentage=4.0 --resign-playthrough=20 --temperature=0.9 --temp-endgame=0.30 --temp-cutoff-move=60 --temp-visit-offset=-0.8 --fpu-strategy=reduction --fpu-value=0.23 --fpu-strategy-at-root=absolute --fpu-value-at-root=1.0 --minimum-kldgain-per-node=0.000030 --black.minimum-kldgain-per-node=0.000048 --policy-softmax-temp=1.4 --resign-wdlstyle=true --noise-epsilon=0.1 --noise-alpha=0.12 --sticky-endgames=true --moves-left-max-effect=0.2 --moves-left-threshold=0.0 --moves-left-slope=0.008 --moves-left-quadratic-factor=1.0 --moves-left-constant-factor=0.0;
   fi
 
   # train a new net
-  ## If the training server is not the local computer, make sure it
-  ## has (sshfs) access to the directory where the client drops the
-  ## training data. This dir is configured in the yaml file
   docker start 18e24368f7d8
-  docker attach 18e24368f7d8
-  export LD_LIBRARY_PATH=/usr/local/cuda/lib64
-  python3 lczero-training/tf/train.py --cfg lczero-training/tf/configs/gpu-monster.yaml --output /mnt/leela-nets/${i}.gz
-  exit ## exits docker only
+  docker exec -it 18e24368f7d8 bash /mnt/train_in_docker.sh $i
 
   ((i=i+1))
 done
