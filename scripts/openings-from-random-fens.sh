ce319dc93fc73e562fedc9161b9935ca717aa325
Hans Ekbrand <hans.ekbrand@gmail.com>
2024-08-16 13:13:30 +0200

    added more script for generating openings from random FENs

---
+++ scripts/openings-from-random-fens.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## openings from random fens
+usage bash openings-from-random-fens.sh 20000 14
+
+number_of_desired_lines=$1
+number_of_pieces=$2
+let number_of_required_intermediate_lines=$1*10
+
+## generate
+cd ~/src/lczero-training/scripts/
+nodejs random-FENs-parent.js $number_of_pieces $number_of_required_intermediate_lines false
+mv ${number_of_pieces}-men-fens.pgn ~/run-q-from-fens-here/
+
+## evaluate
+cd ~/run-q-from-fens-here/
+## locate the latest net
+latest_net=`ls -lStr ~/r-mobility-synthetic/*swa*.gz | tail -n 1 | cut -d " " -f 9`
+## works with a symlink, but the file has to have a newer timestamp than any .txt file in that dir.
+## which is a reason to let file suffix from generation remain .pgn even it is not a pgn when
+## invoked with "false"
+if [[ ! -L $latest_net ]]; then
+  rm *.gz ## remove obsolete link
+  ln -s $latest_net .
+fi
+python3 ~/q-from-fens.py -i ${number_of_pieces}-men-fens.pgn > evaluated-${number_of_pieces}-men-fens.txt
+## creates evaluated-${number_of_pieces}-men-fens.txt
+
+## select
+~/pick-interesting-fens.R $number_of_pieces 0.2
+## creates selected-${number_of_pieces}-men-fens.pgn
+
+## clean up intermediate files
+rm evaluated-${number_of_pieces}-men-fens.txt ${number_of_pieces}-men-fens.pgn
+
+## resulting opening file is now in selected-${number_of_pieces}-men-fens.pgn
---
+++ scripts/pick-interesting-fens.R
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+
+# Get the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# Check if the correct number of arguments is provided
+if (length(args) < 2) {
+  stop("Two arguments must be supplied: number of pieces and delta q", call. = FALSE)
+}
+
+number.of.pieces <- as.numeric(args[1])
+delta.q <- as.numeric(args[2])
+## setwd('/home/hans/mnt/gpu-monster/run-q-from-fens-here/')
+## setwd('/home/hans/run-q-from-fens-here/')
+## print(paste("Will use", 0.5 + delta.q, 0.5 - delta.q, -0.5 + delta.q, "and", -0.5 - delta.q))
+
+foo <- read.table(paste0('evaluated-', number.of.pieces, "-men-fens.txt"), sep = ",")
+these <- which((foo$V2 < 0.5 + delta.q & foo$V2 > 0.5 - delta.q) | (foo$V2 < -0.5 + delta.q & foo$V2 > -0.5 - delta.q))
+cat(paste0('[FEN "', foo$V1[these], '"', "]\n*\n\n"), file = paste0("selected-", number.of.pieces, "-men-fens.pgn"), sep = "")
