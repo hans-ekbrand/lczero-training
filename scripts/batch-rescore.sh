0752fd63978d74b932d55253e9f113326206bea0
Hans Ekbrand <hans.ekbrand@gmail.com>
2024-08-18 02:02:34 +0200

    use all cores when rescoring

--- scripts/batch-rescore.sh
+++ scripts/batch-rescore.sh
@@ -4,5 +4,5 @@ bash ~/src/lczero-training/scripts/unique-ify-by-folder-name-outer.sh
 for i in /home/hans/leela-training-games/lc0/*; do
     echo ${i};
     rmdir --ignore-fail-on-non-empty ${i};
-    ~/src/lc0/build/release/rescorer rescore -t 4 --input=${i} --output=/home/hans/leela-training-games/rescored_synthetic_pool --gaviotatb-paths=/home/hans/gaviota --syzygy-paths=/home/hans/syzygy;
+    ~/src/lc0/build/release/rescorer rescore -t $(nproc --all) --input=${i} --output=/home/hans/leela-training-games/rescored_synthetic_pool --gaviotatb-paths=/home/hans/gaviota --syzygy-paths=/home/hans/syzygy;
 done
