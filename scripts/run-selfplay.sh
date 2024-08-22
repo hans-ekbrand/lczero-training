f9a9799c35a1b213ebe604327ac1419957ed3776
Hans Ekbrand <hans.ekbrand@gmail.com>
2024-08-22 15:34:46 +0200

    set /home/hans on client

--- scripts/run-selfplay.sh
+++ scripts/run-selfplay.sh
@@ -1,1 +1,3 @@
+export XDG_CACHE_HOME=~/mnt/gpu-master/leela-training-games
+
 ~/src/lc0/build/release/lc0 selfplay -w `ls -t ~/r-mobility-synthetic/*swa*.gz | head -n1` --openings-pgn=/home/hans/opening-book.pgn --training --games=-2 --parallelism=64 --minibatch-size=16 --visits=2 --sticky-endgames --gaviotatb-paths=/home/hans/gaviota --temperature=0 --temp-ends=14 --temp-ends-t=14 --openings-mode=shuffled --task-workers=0 --policy-softmax-temp=1.45 --resign-wdlstyle=true --noise-epsilon=0.1 --noise-alpha=0.12 --sticky-endgames=true --moves-left-max-effect=0.2 --moves-left-threshold=0.0 --moves-left-slope=0.007 --moves-left-quadratic-factor=0.85 --moves-left-scaled-factor=0.15 --moves-left-constant-factor=0.0 &> result.log
